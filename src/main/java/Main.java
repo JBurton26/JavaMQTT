ef717639d28d40b73c0481e8297197959f4f5392
JBurton26 <40278490@users.noreply.github.com>
2019-07-10 21:31:53 +0100

    Multiple readings at once

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,31 +8,44 @@ import java.util.Random;
 //Main Class for
 public class Main {
     public static void main(String[] args){
-        MqttClient client;
+
         Random numVals = new Random();
         int intVals = numVals.nextInt(10);
         intVals += 1;
         while (intVals < 5){
             intVals = numVals.nextInt(10);
         }
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("nodeid",numVals.nextInt(5)+1);
-        jsonObject.put("temperature", numVals.nextInt(30));
-        jsonObject.put("humidity", numVals.nextInt(100));
-        try {
-            client = new MqttClient("tcp://192.168.137.45:1883", MqttClient.generateClientId());
-            MqttConnectOptions connOpts = new MqttConnectOptions();
-            connOpts.setCleanSession(true);
-            connOpts.setKeepAliveInterval(1000);
-            connOpts.setUserName("jakepi");
-            connOpts.setPassword("jbpi1234".toCharArray());
-            client.connect(connOpts);
-            MqttMessage message = new MqttMessage();
-            message.setPayload(jsonObject.toString().getBytes());
-            client.publish("IoT_Data", message);
-            client.disconnect();
-        } catch (MqttException e) {
-            e.printStackTrace();
+        messages(intVals);
+    }
+    private static void messages(int num){
+        MqttClient client;
+        Random numVals = new Random();
+        for(int i = 0; i < num; i++){
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("nodeid",numVals.nextInt(5)+1);
+            jsonObject.put("temperature", numVals.nextInt(30));
+            jsonObject.put("humidity", numVals.nextInt(100));
+            try {
+                client = new MqttClient("tcp://192.168.137.45:1883", MqttClient.generateClientId());
+                MqttConnectOptions connOpts = new MqttConnectOptions();
+                connOpts.setCleanSession(true);
+                connOpts.setKeepAliveInterval(1000);
+                connOpts.setUserName("jakepi");
+                connOpts.setPassword("jbpi1234".toCharArray());
+                client.connect(connOpts);
+                MqttMessage message = new MqttMessage();
+                message.setPayload(jsonObject.toString().getBytes());
+                try {
+                    client.publish("IoT_Data", message);
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                client.disconnect();
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
         }
+
     }
 }
